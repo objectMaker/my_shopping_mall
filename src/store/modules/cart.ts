6918473d95330ea5a774658cf47ec962dcc0d67c
objectMaker <528154320@qq.com>
2021-11-21 23:00:28 +0800

    '删除无用的js测试文件'

--- src/store/modules/cart.ts
+++ src/store/modules/cart.ts
@@ -9,7 +9,6 @@ export default {
     ],
   },
   mutations: {
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     editShop(
       state: any,
       payload: {
@@ -17,22 +16,22 @@ export default {
         count: number;
       }
     ) {
-      console.log(payload);
-      // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+      //查找原来购物车有没有包含这个id
       const tempIndex = state.shopIds.findIndex(
         (item: { id: number; count: number }) => {
           return item.id === payload.id;
         }
       );
       if (tempIndex !== -1) {
-        console.log(tempIndex);
+        //如果有取出这一项进行重新赋值
         const temp = state.shopIds[tempIndex];
         temp.count = payload.count;
+        //如果新的数据为0了就从原来数组中删除这一项
         if (temp.count === 0) {
           state.shopIds.splice(tempIndex, 1);
         }
       } else {
-        state.shopIds.push(payload);
+        payload.count > 0 && state.shopIds.push(payload);
       }
     },
   },
--- src/vender/abc.js
+++
@@ -1,2 +0,0 @@
-console.log('fjasjfjsa');
-console.log("dfsaafs");
