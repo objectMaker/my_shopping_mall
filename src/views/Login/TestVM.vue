4fc1830c7b23278f60200c81c73fcbf06f4c0f37
objectMaker <528154320@qq.com>
2021-11-22 19:54:48 +0800

    使用vue3.2的setup写法

--- src/views/Login/TestVM.vue
+++ src/views/Login/TestVM.vue
@@ -3,24 +3,40 @@
   <button @click="changeCount">提交</button>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { ref } from "vue";
-
-export default {
-  props: ["count"],
-  emits: ["update:count"],
-  setup(props, { emit }) {
-    const count1 = ref(0);
-    console.log(props);
-    const changeCount = () => {
-      emit("update:count", count1.value);
-    };
-    return {
-      count1,
-      changeCount,
-    };
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  count: {
+    type: Number,
+    default: 0,
   },
+});
+// eslint-disable-next-line no-undef
+const emit = defineEmits(["update:count"]);
+const count1 = ref(0);
+const changeCount = () => {
+  emit("update:count", count1.value);
 };
 </script>
+<!--<script>-->
+<!--import { ref } from "vue";-->
+
+<!--export default {-->
+<!--  props: ["count"],-->
+<!--  emits: ["update:count"],-->
+<!--  setup(props, { emit }) {-->
+<!--    const count1 = ref(0);-->
+<!--    console.log(props);-->
+<!--    const changeCount = () => {-->
+<!--      emit("update:count", count1.value);-->
+<!--    };-->
+<!--    return {-->
+<!--      count1,-->
+<!--      changeCount,-->
+<!--    };-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
 
 <style></style>
